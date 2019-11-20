ce5ee281c8ff945f4450d66f49ca3d92d2f83907
alepareyes <alejandra.reyesg96@gmail.com>
2019-11-20 13:22:21 +0100

    fixed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Módulo 2: evaluación intermedia.
+
+- [ ] Diagrama de flujo
+- [ ] HTML / CSS
+- [ ] Generar y guardar el número aleatorio
+
+- [ ] Pistas:
+    - [ ] Leer el input
+    - [ ] Comparar con el número aleatorio
+    - [ ] Actualizar la pista
+
+- [ ] Inicializar el contador
+    - [ ] Incrementar el contador cuando hago click en Prueba
--- main.js
+++ main.js
@@ -2,43 +2,64 @@
 
 
 // ELEMENTOS CAMBIANTES E INTERACTIVOS
-const button = document.querySelector(".js-button");
+const button = document.querySelector('.js-button');
 const input = document.querySelector('.js-number');
-const hint = document.querySelector(".js-hint");
+const hint = document.querySelector('.js-hint');
 const numberOfTries = document.querySelector('.js-tries');
+const counterSpan = document.querySelector('.js-tries');
+
+let count = 0;
 
 
+/////////////////RANDOM NUMBER////////////////
 const getRandomNumber = function (max) {
     console.log("Número random generado:");
     return Math.ceil(Math.random() * max);
-};
+}
 
 const randomNumber = getRandomNumber(100);
-
 console.log(randomNumber);
-
-function clickButton() {
-    console.log('click en prueba');
-};
+/////////////////////////////////////////////
 
 
-const algo = function () {
-    if (parseInt(input.value) === randomNumber) {
-        return hint.innerHTML = '¡Lo adivinaste!'
-    } else if (parseInt(input.value) > randomNumber) {
-        return hint.innerHTML = 'Demasiado alto'
-    } else if (parseInt(input.value) < randomNumber) {
-        return hint.innerHTML = 'Demasiado bajo...'
-    }
-
-};
-
+/////////////////HANDLE BUTTON///////////////
 const handlerEvent = function (ev) {
     ev.preventDefault();
-    algo();
+    compareUserNumber();
 }
-/* click boton sobre formulario
-se refresca página
-prevent default; */
+/////////////////////////////////////////////
 
+
+
+/////////////COMPARE NUMBER & HITS ABOUT NUMBER////////
+const compareUserNumber = function () {
+    const inputNumberValue = parseInt(input.value);
+    if (inputNumberValue < 1 || inputNumberValue > 100) {
+        hint.innerHTML = 'El número debe estar entre el 1 y 100';
+    } else if (inputNumberValue > randomNumber) {
+        incCounter();
+        hint.innerHTML = 'Demasiado alto';
+    } else if (inputNumberValue < randomNumber) {
+        incCounter();
+        hint.innerHTML = 'Demasiado bajo...';
+    } else if (inputNumberValue === randomNumber) {
+        incCounter();
+        hint.innerHTML = '¡Lo adivinaste!';
+    }
+}
+//////////////////////////////////////////////////////////
+/* function updateHint(text) {
+    hint.ineerHTML = text;
+} */
+
+
+
+///////////////////////COUNTER////////////////////////////
+function incCounter() {
+    count += 1;
+    counterSpan.innerHTML = count;
+}
+//////////////////////////////////////////////////////////
+
+//////////////////////LISTEN EVENTS//////////////////////
 button.addEventListener('click', handlerEvent);

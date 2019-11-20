008bd2d0942489ed25e44de7edce1b473313d46a
alepareyes <alejandra.reyesg96@gmail.com>
2019-11-20 08:49:10 +0100

    add html and styles

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+
+// ELEMENTOS CAMBIANTES E INTERACTIVOS
+
+const button = document.querySelector(".js-number");
+
+
+const input = document.querySelector('js-number');
+
+
+const hint = document.querySelector("js-hint");
+
+const numberoftries = document.querySelector('js-tries');
+
+function
+
+    button.addEventListener('click')
